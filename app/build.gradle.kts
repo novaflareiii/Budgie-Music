d3921b4bfb281f4a353b65a9909dceca5f3d5128
novaflare <mridanshugupta@gmail.com>
2026-09-07 11:23:33 +0000

    Rebrand app to Budgie Music with custom icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         val baseVersionName = requireNotNull(versionName)
         buildConfigField("String", "BASE_VERSION_NAME", "\"$baseVersionName\"")
         buildCommit?.let { versionName = "$baseVersionName+$it" }
-        resValue("string", "app_name", appNameOverride ?: "Metrolist")
+        resValue("string", "app_name", appNameOverride ?: "Budgie Music")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
@@ -133,7 +133,7 @@ android {
             }
             isDebuggable = true
             if (appNameOverride == null) {
-                resValue("string", "app_name", "Metrolist Debug")
+                resValue("string", "app_name", "Budgie Music Debug")
             }
             signingConfig =
                 if (workflowDebugKeystoreFile != null) {
